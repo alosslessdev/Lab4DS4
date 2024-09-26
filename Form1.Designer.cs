3bf1ef1246f4005c40273b5b91b4098d45a47259
Shiro2550 <shiro2550@gmail.com>
2024-09-26 15:52:57 -0700

    cuartos

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,26 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
+            this.Encabezado = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Cuartos = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Encabezado)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cuartos)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(65, 100);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(201, 38);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Hotel Otaku";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Encabezado
+            // 
+            this.Encabezado.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Encabezado.Image = ((System.Drawing.Image)(resources.GetObject("Encabezado.Image")));
+            this.Encabezado.Location = new System.Drawing.Point(0, -2);
+            this.Encabezado.Name = "Encabezado";
+            this.Encabezado.Size = new System.Drawing.Size(778, 152);
+            this.Encabezado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Encabezado.TabIndex = 1;
+            this.Encabezado.TabStop = false;
+            this.Encabezado.Click += new System.EventHandler(this.Encabezado_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Individual",
+            "Doble",
+            "Suite"});
+            this.comboBox1.Location = new System.Drawing.Point(0, 173);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Cuartos
+            // 
+            this.Cuartos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Cuartos.Location = new System.Drawing.Point(223, 205);
+            this.Cuartos.Name = "Cuartos";
+            this.Cuartos.Size = new System.Drawing.Size(504, 385);
+            this.Cuartos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Cuartos.TabIndex = 3;
+            this.Cuartos.TabStop = false;
+            this.Cuartos.Click += new System.EventHandler(this.Cuartos_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1072, 602);
+            this.ClientSize = new System.Drawing.Size(778, 602);
+            this.Controls.Add(this.Cuartos);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.Encabezado);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "lbHotel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Encabezado)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cuartos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,6 +112,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox Encabezado;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox Cuartos;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,53 @@ namespace Laboratorio_4
         public Form1()
         {
             InitializeComponent();
+
+
+
+            // Enlazar el evento SelectedIndexChanged al ComboBox
+            comboBox1.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Cambiar imagen según la selección del ComboBox
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0: // Individual
+                    Cuartos.Image = Image.FromFile("C:\\Users\\Ivan\\OneDrive - Universidad Tecnológica de Panamá\\2 Year #2 Semester\\Desarrollo de Software 4\\trabajos\\Laboratorio#4\\Laboratorio#4\\Individual.jpg");
+                    break;
+                case 1: // Doble
+                    Cuartos.Image = Image.FromFile("C:\\Users\\Ivan\\OneDrive - Universidad Tecnológica de Panamá\\2 Year #2 Semester\\Desarrollo de Software 4\\trabajos\\Laboratorio#4\\Laboratorio#4\\Doble.jpg");
+                    break;
+                case 2: // Suite
+                    Cuartos.Image = Image.FromFile("C:\\Users\\Ivan\\OneDrive - Universidad Tecnológica de Panamá\\2 Year #2 Semester\\Desarrollo de Software 4\\trabajos\\Laboratorio#4\\Laboratorio#4\\Suite.jpg");
+                    break;
+            }
+        }
+
+        private void Cuartos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Encabezado_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
