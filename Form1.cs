910805d5c2433dc8b7a16b81e480d0007648c28c
Shiro2550 <shiro2550@gmail.com>
2024-09-26 13:56:31 -0700

    test

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,14 @@
-namespace Lab4DS4
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorio_4
 {
     public partial class Form1 : Form
     {
