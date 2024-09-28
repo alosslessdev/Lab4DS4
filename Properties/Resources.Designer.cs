be9ce694ca2b19a04aeaf197e06c4fbc0539ff59
alosslessdev <ppaanncchhoo507@gmail.com>
2024-09-28 17:54:18 -0500

    hhj

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Laboratorio_4.Properties {
+namespace Lab4v2.Properties {
     using System;
     
     
